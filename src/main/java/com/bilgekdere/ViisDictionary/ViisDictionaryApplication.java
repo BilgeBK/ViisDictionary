987873d22ba843915f7fcc12a146d8e4bf7ac403
BilgeBK <bilgekizgindere@gmail.com>
2026-01-02 21:45:23 +0300

    Initial commit

---
+++ src/main/java/com/bilgekdere/ViisDictionary/ViisDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.bilgekdere.ViisDictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViisDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViisDictionaryApplication.class, args);
+	}
+
+}
